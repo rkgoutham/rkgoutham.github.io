df270f9ef0df17e86320a8ab97232ee3db51a560
Goutham <gouthamrk1@yahoo.com>
2018-05-21 17:15:13 -0700

    change

--- CrossOriginScript.js
+++ CrossOriginScript.js
@@ -90,8 +90,8 @@ function doPost() {
         var headers = {};
         if (isJson) {
             headers = {
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
+                Accept: 'application/json'
+                //‘Content-Type': 'application/json'
             }
         }
         var params = {
